1c400ea93e9a2f254ccf3dc866bd6fb3a2501520
loveyxw <104673125+loveyxw@users.noreply.github.com>
2023-03-09 23:55:36 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -11,6 +11,6 @@ export const defaultMessage = `
 
 - 点右下角⚙输入你的key
 
-- 联系我 wechat：97220914
+- 如需key请加微信:97220914
 
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
