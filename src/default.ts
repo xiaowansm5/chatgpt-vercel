034dbd8e583ab81fac21cd1c8d93027a4f03adc6
xiaowansm <31873594+xiaowansm5@users.noreply.github.com>
2023-03-13 13:49:24 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -7,10 +7,12 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 本镜像站需要填入自己的 key 才可使用。
+- 在您开始使用ChatGPT镜像站之前，我们需要你提供一个API Key，API Key来自OpenAI。
+
+- 你可以使用OpenAI账号获取API Key，[查询API Key链接](https://platform.openai.com/account/api-keys)
 
 - 点右下角⚙输入你的key
 
-- 如需key请加微信:[xiaowan_sm](https://pic.hhao.cc/imgs/2023/03/6c4f8dab0ac56e62.jpg)
+- 如需**专属镜像站和机器人**请联系我们:[微信](https://pic.hhao.cc/imgs/2023/03/6c4f8dab0ac56e62.jpg) | [邮箱](mailto:wwang.pw@gmail.com)
 
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
