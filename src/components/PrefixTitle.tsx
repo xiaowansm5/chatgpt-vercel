884822a55c3420c96c37c0c4e444f5380d0f551c
xiaowansm <31873594+xiaowansm5@users.noreply.github.com>
2023-06-16 08:38:28 +0800

    Update PrefixTitle.tsx

--- src/components/PrefixTitle.tsx
+++ src/components/PrefixTitle.tsx
@@ -2,5 +2,5 @@ import type { JSXElement } from "solid-js"
 import { Title } from "solid-start"
 
 export default function (props: { children?: JSXElement }) {
-  return <Title>ChatGPT{props.children ? " | " + props.children : ""}</Title>
+  return <Title>时光AI{props.children ? " | " + props.children : ""}</Title>
 }
